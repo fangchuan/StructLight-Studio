37e8cd2a2fa44c47469bd3343dc099b527a27f42
fangchuan <1457737815@qq.com>
2017-08-01 13:27:13 +0800

    修改EncoderPhaseShift3()，调整decodeFrames中mask值

--- codec/CodecPhaseShift3.cpp
+++ codec/CodecPhaseShift3.cpp
@@ -18,26 +18,22 @@ EncoderPhaseShift3::EncoderPhaseShift3(unsigned int _screenCols, unsigned int _s
     for(unsigned int i=0; i<N; i++){
         float phase = 2.0*pi/float(N) * i;
         float pitch = screenCols;
-//        cv::Mat patternI = pstools::computePhaseVector(screenCols, phase, pitch);
-        // Loop through vector
-        cv::Mat phaseVector(screenCols, 1, CV_32F);
-        for(int i=0; i<phaseVector.rows; i++){
-            // Amplitude of channels
-            float amp = 0.5*(1+cos(2*pi*i/pitch - phase));
-            phaseVector.at<float>(i, 0) = amp;
-        }
-        phaseVector = phaseVector.t();
-        // Repeat pattern
-        phaseVector = cv::repeat(phaseVector, screenRows, 1);
-//        // Add noise
-//        cv::Mat noise(phaseVector.size(), CV_32F);
-//        cv::randn(noise, 0.0, 0.05);
-//        phaseVector += noise;
-        phaseVector.convertTo(phaseVector, CV_8U, 255.0);
-        // Repeat texture
-        cv::Mat patternI(screenRows, screenCols, CV_8UC3);
-        cv::cvtColor(phaseVector, patternI, cv::COLOR_GRAY2RGB);
-        patterns.push_back(patternI);
+        cv::Mat patternI = pstools::computePhaseVector(screenCols, phase, pitch);
+//         // Loop through vector
+//         cv::Mat phaseVector(screenCols, 1, CV_32F);
+//         for(int i=0; i<phaseVector.rows; i++){
+//             // Amplitude of channels
+//             float amp = 0.5*(1+cos(2*pi*i/pitch - phase));
+//             phaseVector.at<float>(i, 0) = amp;
+//         }
+//         phaseVector = phaseVector.t();
+//         // Repeat pattern
+//         phaseVector = cv::repeat(phaseVector, screenRows, 1);
+//         phaseVector.convertTo(phaseVector, CV_8U, 255.0);
+//         // Repeat texture
+//         cv::Mat patternI(screenRows, screenCols, CV_8UC3);
+//         cv::cvtColor(phaseVector, patternI, cv::COLOR_GRAY2RGB);
+        patterns.push_back(patternI.t());
     }
 }
 
@@ -78,20 +74,19 @@ void DecoderPhaseShift3::decodeFrames(cv::Mat &up, cv::Mat &vp, cv::Mat &mask, c
     // Create mask from modulation image and erode
     mask.create(shading.size(), cv::DataType<bool>::type);
     //mask = (shading > avg) & (shading > 55) & (shading < 250);
-    mask = shading > 10;
+    mask = shading > 30;
 
     cv::Mat dx, dy;
-    cv::Sobel(cv::Mat_<float>(shading), dx, -1, 1, 0, 3);
-    cv::Sobel(cv::Mat_<float>(shading), dy, -1, 0, 1, 3);
-    cv::Mat edgesShading = abs(dx) + abs(dy);
+//     cv::Sobel(cv::Mat_<float>(shading), dx, -1, 1, 0, 3);
+//     cv::Sobel(cv::Mat_<float>(shading), dy, -1, 0, 1, 3);
+//     cv::Mat edgesShading = abs(dx) + abs(dy);
 //    cv::magnitude(dx, dy, edgesShading);
-
     cv::Sobel(up, dx, -1, 1, 0, 3);
     cv::Sobel(up, dy, -1, 0, 1, 3);
-    cv::Mat edgesUp = abs(dx) + abs(dy);
-//    cv::magnitude(dx, dy, edgesUp);
+    cv::Mat edgesUp ;
+    cv::magnitude(dx, dy, edgesUp);
 
 //cvtools::writeMat(edges, "edges.mat", "edges");
-    mask = mask &  (edgesUp < 80);
+    mask = mask &  (edgesUp < 75);
 
 }
